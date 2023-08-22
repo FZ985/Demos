10b3aff58cd515953b9696e361fc8fe614f165f2
945100985@qq.com <945100985@qq.com>
2023-08-22 11:42:12 +0800

    add click listener

--- app/src/main/java/com/demos/beziertest/TestBezierActivity.kt
+++ app/src/main/java/com/demos/beziertest/TestBezierActivity.kt
@@ -1,7 +1,9 @@
 package com.demos.beziertest
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.demos.click.PerfectClickListener
 import com.demos.databinding.ZzTestBezierBinding
 
 
@@ -19,9 +21,10 @@ class TestBezierActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        binding.click.setOnClickListener {
-            binding.bv.start()
-        }
+        binding.click.setOnClickListener(object : PerfectClickListener() {
+            override fun onViewClick(v: View) {
+                binding.bv.start()
+            }
+        })
     }
 }
