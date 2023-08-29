cd72c273d214abc6d6e579193f449b7bb94ebe6c
945100985@qq.com <945100985@qq.com>
2023-08-29 14:04:07 +0800

    remove invalid log

--- app/src/main/java/com/demos/MainActivity.java
+++ app/src/main/java/com/demos/MainActivity.java
@@ -15,7 +15,6 @@ import com.demos.other.AppbarLayoutActivity;
 import com.demos.prizedraw.PrizeDrawActivity;
 import com.demos.span.SpanActivity;
 import com.demos.textview.PicTextActivity;
-import com.demos.utils.SHA256Encryption;
 import com.demos.viewpager.toplinkcustom.TopLinkCustomActivity;
 import com.demos.viewpager.toplinkmagic.TopLinkMagicActivity;
 
@@ -40,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
         click(binding.appbar, AppbarLayoutActivity.class);
         click(binding.testbezirer, TestBezierActivity.class);
 
-        SHA256Encryption.main(null);
     }
 
     private void click(View view, Class<?> cls) {
